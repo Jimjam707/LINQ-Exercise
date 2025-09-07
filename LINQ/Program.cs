399d0b28b696a49216c613f6241f13a90ab829b3
Jimi Adams <jimi-8@live.com>
2025-09-07 12:39:50 -0400

    ordered video game names

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Video games in order by name length: ");
+            Console.WriteLine();
+
+            var names = new List<string>
+            {
+                "Mario Kart", "Stardew Valley", "Oldschool RuneScape", "Deadlock", "007: GoldenEye", "Raft",
+                "Hunt: Showdown", "Escape from Tarkov",
+            };
+            
+            var sortedNames = names.OrderBy(name => name.Length).ToList();
+            
+            sortedNames.ForEach(name => Console.WriteLine(name));
         }
     }
 }
